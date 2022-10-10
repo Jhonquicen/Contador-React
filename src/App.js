46e8ff3048d8f015a11d9b860c73c50b1370496f
ferney quiceno <104113942+Jhonquicen@users.noreply.github.com>
2022-10-10 11:17:14 -0500

    Contador-React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Counter from "./Counter/Counter";
+
+
+class App extends React.Component {
+
+render() {
+  return (
+    <div className="App">
+      <h1>Contador</h1>
+    
+      <Counter />
+
+    </div>
+  );
+}
+}
+export default App;
